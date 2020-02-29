e98fe25598dfdbd6ef9040539a5754d686f08367
hhernandez008 <hhernandez008@gmail.com>
2020-02-28 17:08:42 -0800

    Added homepage style to the child theme and uploaded new image.

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -13,9 +13,10 @@
 // Enqueue scripts and styles
 
 function accelerate_child_scripts(){
-  wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('accelerate-style', get_template_directory_uri() . '/style.css');
 
-  wp_enqueue_style( 'child-style', get_stylesheet_uri() . '/style.css', array ('accelerate-style'));
+  wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css',
+   array ('accelerate-style'));
 }
 
 add_action('wp_enqueue_scripts', 'accelerate_child_scripts');
